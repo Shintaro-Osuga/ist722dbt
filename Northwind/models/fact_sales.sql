16c3d4c4800d6300bc3eab639f2b9bd846cc1ffa
Shintaro Osuga <sosuga@syr.edu>
2025-03-17 00:47:20 +0000

    Finished assignment F

---
+++ Northwind/models/fact_sales.sql
@@ -0,0 +1,27 @@
+with stg_orders as
+(
+    select 
+        OrderId,
+        {{ dbt_utils.generate_surrogate_key(["customerid"]) }} as customerkey,
+        {{ dbt_utils.generate_surrogate_key(["employeeid"]) }} as employeekey,
+        replace(to_date(orderdate)::varchar,'-','')::int as orderdatekey,
+    from {{source("northwind","Orders")}}
+),
+stg_order_details as
+(
+    select
+        orderid, 
+        {{ dbt_utils.generate_surrogate_key(["productid"]) }} as productkey,
+        sum(Quantity) as quantity,
+        sum(Quantity*UnitPrice) as extendedpriceamount,
+        sum(Quantity*UnitPrice*Discount) as discountamount,
+        sum((Quantity*UnitPrice) - (Quantity*UnitPrice*Discount)) as soldamount
+    from {{source("northwind","Order_Details")}}
+    group by orderid, productid
+)
+select 
+    o.*,
+    od.productkey, od.quantity, od.extendedpriceamount,
+    od.discountamount, od.soldamount
+    from stg_orders o
+        join stg_order_details od on o.orderid = od.orderid
